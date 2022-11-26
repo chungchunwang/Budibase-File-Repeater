dfb2c49e64729d9b94615bb9a4bfc840d3ac259f
chungchunwang <62127500+chungchunwang@users.noreply.github.com>
2022-11-26 15:25:47 +0800

    Changes and README

---
+++ src/Component.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { getContext } from "svelte"
+
+  export let data;
+
+  const { styleable, Provider  } = getContext("sdk")
+  const component = getContext("component")
+
+  let filesURL = [];
+    $: filesURL = data? JSON.parse(data).map((e) => e.link): [];
+</script>
+
+<div use:styleable={$component.styles}>
+  {#each filesURL as url}
+        <Provider data={{fileURL:url}}>
+        <slot />
+      </Provider>
+      {/each}
+</div>
